333a49fc71e5e714373c45f7120da24b3001efdc
Qwerty123-25 <ntru7108@gmail.com>
2025-07-27 01:32:05 +0300

    2nd attempt merge branch

--- src/first_py_task/main.py
+++ src/first_py_task/main.py
@@ -4,22 +4,16 @@ import requests
 import json
 
 r = requests.get('https://api.github.com/user', auth=('user', 'pass')) #создаем запрос
-
-
+s = r.json()
 
 
 print(r.status_code)
 print(r.headers['content-type'])
 print(r.encoding)
 
-s = r.json()
+
 print(s['message'])
 print(s['documentation_url'])
 print(s['status'])
-
 for k, v in s.items():
     print(f"{k}: {v}")
-
-
-
-
